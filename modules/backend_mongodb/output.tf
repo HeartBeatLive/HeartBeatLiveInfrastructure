04c6acdd8f15f34cae2e544566a18ee30b673ea7
Nikita Ivchenko <munoongg@gmail.com>
2022-07-24 14:52:26 +0300

    Add MongoDB password encoding in connection URI

--- modules/backend_mongodb/output.tf
+++ modules/backend_mongodb/output.tf
@@ -1,6 +1,6 @@
 output "backend_application_credentials" {
   value = {
-    uri                = "${replace(mongodbatlas_cluster.main.connection_strings[0].private_srv, "mongodb+srv://", "mongodb+srv://${mongodbatlas_database_user.application_db_user.username}:${mongodbatlas_database_user.application_db_user.password}@")}/${local.application_db_name}"
+    uri                = "${replace(mongodbatlas_cluster.main.connection_strings[0].private_srv, "mongodb+srv://", "mongodb+srv://${mongodbatlas_database_user.application_db_user.username}:${urlencode(mongodbatlas_database_user.application_db_user.password)}@")}/${local.application_db_name}"
     auth_database_name = mongodbatlas_database_user.application_db_user.auth_database_name
   }
 }
